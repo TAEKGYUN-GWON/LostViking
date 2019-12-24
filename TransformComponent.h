0e1edad57d3f63526178d33536a57ad3a1975cdc
JiHo_Jung <jhhoya4072@naver.com>
2019-12-24 16:55:02 +0900

    Update TransformComponent.h

--- TransformComponent.h
+++ TransformComponent.h
@@ -4,7 +4,6 @@ class Object;
 class TransformComponent
 {
 private:
-	RECT _rc;
 	Object *_object;
 	Vector2 _pos;
 	Vector2 _scale;
@@ -16,6 +15,5 @@ public:
 	inline Vector2 GetPos() { return _pos; }
 	inline Vector2 GetScale() { return _scale; }
 	inline void SetPos(Vector2 pos) { _pos = pos; }
-	inline RECT* getRect() { return &_rc; }
 };
 
