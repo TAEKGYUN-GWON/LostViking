dc5a13765edf04307a60e965619d4a9347cc9226
TAEKGYUN-GWON <god_lion@naver.com>
2019-12-26 03:18:44 +0900

    one Vector2 (1, 1)에서 (1, -1)로 수정 api 기준 y축 반대니까

--- Vector2.cpp
+++ Vector2.cpp
@@ -5,8 +5,7 @@ const Vector2 Vector2::up = Vector2(0, -1);
 const Vector2 Vector2::down = Vector2(0, 1);
 const Vector2 Vector2::left = Vector2(-1, 0);
 const Vector2 Vector2::right = Vector2(1, 0);
-const Vector2 Vector2::one = Vector2(1, 1);
-
+const Vector2 Vector2::one = Vector2(1, -1);
 
 Vector2 Vector2::operator-(Vector2 to)
 {
