09832444c78844ccf07f2c565c538556c9ca7065
leehn5 <31524982+leehn5@users.noreply.github.com>
2019-12-26 16:41:12 +0900

    Graphic, GraphicsManager 내용 수정 이제 frameImage 할 수 있음

--- Graphic.cpp
+++ Graphic.cpp
@@ -24,7 +24,7 @@ HRESULT Graphic::Init(ID2D1Bitmap* bitmap)
 	return S_OK;
 }
 
-HRESULT Graphic::Init(ID2D1Bitmap * bitmap, int frameX, int frameY)
+HRESULT Graphic::Init(ID2D1Bitmap * bitmap, int maxFrameX, int maxFrameY)
 {
 	_graphicInfo = new GRAPHIC_INFO;
 	_graphicInfo->bitmap = bitmap;
@@ -35,16 +35,29 @@ HRESULT Graphic::Init(ID2D1Bitmap * bitmap, int frameX, int frameY)
 	_graphicInfo->scale = Vector2(1.0f, 1.0f);
 	_graphicInfo->alpha = 1.0f;
 	_graphicInfo->angle = 0.0f;
-	_graphicInfo->frameWidth = _graphicInfo->size.x / frameX;
-	_graphicInfo->frameHeight = _graphicInfo->size.y / frameY;
-	_graphicInfo->maxFrameX = frameX - 1;
-	_graphicInfo->maxFrameY = frameY - 1;
-
+	_graphicInfo->frameWidth = _graphicInfo->size.x / maxFrameX;
+	_graphicInfo->frameHeight = _graphicInfo->size.y / maxFrameY;
+	_graphicInfo->maxFrameX = maxFrameX;
+	_graphicInfo->maxFrameY = maxFrameY;
 
 	if (_graphicInfo->bitmap == nullptr)
 	{
 		Release();
 		return E_FAIL;
+	}
+
+
+	WICRect rc;
+	for (int i = 0; i < _graphicInfo->maxFrameY; ++i)
+	{
+		for (int j = 0; j < _graphicInfo->maxFrameX; ++j)
+		{
+			rc.X = _graphicInfo->frameWidth * j;
+			rc.Y = _graphicInfo->frameHeight * i;
+			rc.Width = _graphicInfo->frameWidth;
+			rc.Height = _graphicInfo->frameHeight;
+			_vFrameRect.push_back(rc);
+		}
 	}
 
 	return S_OK;
@@ -70,21 +83,30 @@ void Graphic::Render(float x, float y)
 	D2D1_RECT_F dxArea = RectF(-_graphicInfo->size.x / 2, -_graphicInfo->size.y / 2, _graphicInfo->size.x / 2, _graphicInfo->size.y / 2);
 
 	_RT->SetTransform(Matrix3x2F::Identity() * rotation * trans);
-	if(_graphicInfo->bitmap) _RT->DrawBitmap(_graphicInfo->bitmap, dxArea);
+	if(_graphicInfo->bitmap) _RT->DrawBitmap(_graphicInfo->bitmap, dxArea, _graphicInfo->alpha);
+
 }
 
 void Graphic::FrameRender(float x, float y, int curFrameX, int curFrameY)
 {
+	int currentFrameX = curFrameX;
+	int currentFrameY = curFrameY;
+
+	if (currentFrameX > _graphicInfo->maxFrameX - 1) currentFrameX = _graphicInfo->maxFrameX - 1;
+	if (currentFrameY > _graphicInfo->maxFrameY - 1) currentFrameY = _graphicInfo->maxFrameY - 1;
+
+	int frame = currentFrameY * _graphicInfo->maxFrameX + currentFrameX;
+
 	_graphicInfo->size.x *= _graphicInfo->scale.x;
 	_graphicInfo->size.y *= _graphicInfo->scale.y;
 
 	Matrix3x2F rotation = Matrix3x2F::Rotation(_graphicInfo->angle, Point2F());
 	Matrix3x2F trans = Matrix3x2F::Translation(x, y);
 
-	D2D1_RECT_F dxArea = RectF(-_graphicInfo->size.x / 2, -_graphicInfo->size.y / 2, _graphicInfo->size.x / 2, _graphicInfo->size.y / 2);
+	D2D1_RECT_F dxArea = RectF(-_graphicInfo->size.x / 2 / (_graphicInfo->maxFrameX), -_graphicInfo->size.y / 2, _graphicInfo->size.x / 2 / (_graphicInfo->maxFrameX), _graphicInfo->size.y / 2);
 
-	D2D1_RECT_F dxSrc = RectF(-_graphicInfo->size.x / 2, -_graphicInfo->size.y / 2, _graphicInfo->size.x / 2, _graphicInfo->size.y / 2);
+	D2D1_RECT_F dxSrc = RectF(_vFrameRect[frame].X, _vFrameRect[frame].Y, _vFrameRect[frame].X + _vFrameRect[frame].Width, _vFrameRect[frame].Y + _vFrameRect[frame].Height);
 
 	_RT->SetTransform(Matrix3x2F::Identity() * rotation * trans);
-	if (_graphicInfo->bitmap) _RT->DrawBitmap(_graphicInfo->bitmap, &dxArea, _graphicInfo->angle, D2D1_BITMAP_INTERPOLATION_MODE_LINEAR, &dxSrc);
+	if (_graphicInfo->bitmap) _RT->DrawBitmap(_graphicInfo->bitmap, &dxArea, _graphicInfo->alpha, D2D1_BITMAP_INTERPOLATION_MODE_LINEAR, &dxSrc);
 }
--- playGround.h
+++ playGround.h
@@ -13,16 +13,24 @@ private:
 	//테스트용 변수
 	bool _tested;
 
+	int _curFrameX;
+	int _curFrameY;
+	int _count;
+
 	Graphic* _img;
+	Graphic* _img2;
+	Graphic* _img3;
 	
 public:
 	playGround();
 	~playGround();
 
-	virtual HRESULT init();			//초기화 전용 함수
-	virtual void release();			//메모리 해제 함수
-	virtual void update();			//연산 전용 함수
-	virtual void render();	//그리기 전용 함수
+	virtual HRESULT init();
+	virtual void release();
+	virtual void update();
+	virtual void render();
+
+	void FrameAnimation();
 
 };
 
